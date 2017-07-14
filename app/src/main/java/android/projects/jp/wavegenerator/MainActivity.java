670df64ff9d5bc1385aaa84f4118078e689b545d
joanaMCSP <joana.mcsp@gmail.com>
2017-07-14 12:43:56 +0100

    Basic screen structure

--- app/src/main/java/android/projects/jp/wavegenerator/MainActivity.java
+++ app/src/main/java/android/projects/jp/wavegenerator/MainActivity.java
@@ -1,13 +1,123 @@
 package android.projects.jp.wavegenerator;
 
+import android.projects.jp.wavegenerator.wave_generators.SineWaveGenerator;
+import android.projects.jp.wavegenerator.wave_generators.helpers.AudioSettings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SineWaveGenerator sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sw = new SineWaveGenerator();
+        bindControls();
+    }
+
+    private void bindControls(){
+
+        final TextView currentFrequency = (TextView) findViewById(R.id.frequency);
+        currentFrequency.setText(String.valueOf(AudioSettings.DEFAULT_FREQUENCY));
+
+        Button buttonFrequencyIncrease = (Button) findViewById(R.id.buttonFrequencyIncrease);
+        buttonFrequencyIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int frequency = Integer.parseInt(currentFrequency.getText().toString());
+                if(frequency + AudioSettings.FREQ_STEP <= AudioSettings.MAX_FREQ){
+                    frequency += AudioSettings.FREQ_STEP;
+                    sw.setFrequency(frequency);
+                    String displayText = frequency + "";
+                    currentFrequency.setText(displayText);
+                }
+            }
+        });
+
+        Button buttonFrequencyDecrease = (Button) findViewById(R.id.buttonFrequencyDecrease);
+        buttonFrequencyDecrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int frequency = Integer.parseInt(currentFrequency.getText().toString());
+                if(frequency - AudioSettings.FREQ_STEP >= AudioSettings.MIN_FREQ){
+                    frequency  -= AudioSettings.FREQ_STEP;
+                    sw.setFrequency(frequency);
+                    String displayText = frequency + "";
+                    currentFrequency.setText(displayText);
+                }
+            }
+        });
+
+        final TextView currentVolumeLeft = (TextView) findViewById(R.id.currentVolumeLeft);
+        currentVolumeLeft.setText(String.valueOf(AudioSettings.DEFAULT_VOLUME));
+
+        final TextView currentVolumeRight = (TextView) findViewById(R.id.currentVolumeRight);
+        currentVolumeRight.setText(String.valueOf(AudioSettings.DEFAULT_VOLUME));
+
+        final SeekBar volumeLeftSlider = (SeekBar) findViewById(R.id.volumeLeftSlider);
+        volumeLeftSlider.setMax(AudioSettings.DEVICE_CLIPPING_POINT_IN_DB);
+        volumeLeftSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                currentVolumeLeft.setText(String.valueOf(progress));
+                sw.setLeftVolume(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final SeekBar volumeRightSlider = (SeekBar) findViewById(R.id.volumeRightSlider);
+        volumeRightSlider.setMax(AudioSettings.DEVICE_CLIPPING_POINT_IN_DB);
+        volumeRightSlider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                currentVolumeRight.setText(String.valueOf(progress));
+                sw.setRightVolume(progress);
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        final Button buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        buttonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(sw.isPlaying()){
+                    sw.stop();
+                    buttonPlay.setText("Play");
+                } else {
+                    sw.playTone();
+                    buttonPlay.setText("Stop");
+                }
+            }
+        });
     }
 }
--- app/src/main/java/android/projects/jp/wavegenerator/wave_generators/helpers/AudioSettings.java
+++ app/src/main/java/android/projects/jp/wavegenerator/wave_generators/helpers/AudioSettings.java
@@ -8,11 +8,17 @@ import android.media.AudioTrack;
 
 public final class AudioSettings {
 
+    public static final double SAMPLE_RATE = 44100.0;
     public static final short MAX_AMPLITUDE = Short.MAX_VALUE; // 16 bit audio
-    public static final int NUMBER_OF_CHANNELS = 2; //stereo
-    public static final int BLOCK_ALIGN = Short.SIZE / 2 * NUMBER_OF_CHANNELS; //number of bytes per frame
-    public static final double GENERATED_AUDIO_DURATION_IN_SECONDS = 0.01;
     public static final int DEVICE_CLIPPING_POINT_IN_DB = 96; //in 16-bit audio
     public static final double MAX_NORMALIZED_AMPLITUDE = 1.0;
+    public static final int NUMBER_OF_CHANNELS = 2; //stereo
+    public static final int BLOCK_ALIGN = 2 * NUMBER_OF_CHANNELS; //number of bytes per frame
+    public static final double GENERATED_AUDIO_DURATION_IN_SECONDS = 0.1;
+    public static final int DEFAULT_VOLUME = 0;
+    public static final int DEFAULT_FREQUENCY = 500;
+    public static final int MAX_FREQ = 20000;
+    public static final int MIN_FREQ = 0;
+    public static final int FREQ_STEP = 100;
 
 }
